0f759078ebb5faacd03db638231d2ee011e0160f
Hervé BRY <herve.bry@geneanet.org>
2026-01-14 15:36:46 +0100

    Release 1.0.7

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0.6',
+    version='1.0.7',
 
     description='SSL certificates and keys manager',
     long_description="CertProxy centralizes SSL certificates and key management, allowing certificates to be fetched only once from an ACME authority (like Let's Encrypt) and distributed to many servers.",
